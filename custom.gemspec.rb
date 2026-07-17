fd786c36c00a3641cf79c07acdd9a7d96b544a3c
nordlet-ci <ci@nordlet.com>
2026-07-17 17:13:53 +0000

    Release v0.1.2

--- custom.gemspec.rb
+++ custom.gemspec.rb
@@ -1,16 +1,10 @@
 # frozen_string_literal: true
 
-# Custom gemspec configuration file
-# This file is automatically loaded by the main gemspec file. The 'spec' variable is available
-# in this context from the main gemspec file. You can modify this file to add custom metadata,
-# dependencies, or other gemspec configurations. If you do make changes to this file, you will
-# need to add it to the .fernignore file to prevent your changes from being overwritten.
-
 def add_custom_gemspec_data(spec)
-  # Example custom configurations (uncomment and modify as needed)
-
-  # spec.authors = ["Your name"]
-  # spec.email = ["your.email@example.com"]
-  # spec.homepage = "https://github.com/your-org/nordlet-ruby"
-  # spec.license = "Your license"
+  spec.license = "MIT"
+  spec.homepage = "https://github.com/nordlet/nordlet-sdk-ruby"
+  spec.metadata["source_code_uri"] = spec.homepage
+  spec.files = Dir.chdir(__dir__) do
+    Dir["lib/**/*", "README.md", "reference.md"].select { |f| File.file?(f) }
+  end
 end
--- lib/nordlet/version.rb
+++ lib/nordlet/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Nordlet
-  VERSION = "0.0.0"
+  VERSION = "0.1.2"
 end
